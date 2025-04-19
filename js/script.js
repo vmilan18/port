904e8b74f6f3d1b8699a5fd66300c6ba97919555
vmilan18 <vmilan550@gmail.com>
2025-04-20 00:50:34 +0530

    js commit

--- js/script.js
+++ js/script.js
@@ -86,7 +86,7 @@ async function displayAlbums() {
     let array = Array.from(anchors)
     for (let index = 0; index < array.length; index++) {
         const e = array[index];
-        if (e.href.includes("/song/")) {
+        if (e.href.includes("/song/") && !e.href.includes(".htaccess")) {
             let folder = e.href.split("/").slice(-1)[0];
 
             // Get the metadata of the folder
